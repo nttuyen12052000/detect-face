467ee465932874fdd94ad064be9111a73e299546
NTT <nttuyen12052000>
2020-03-28 12:03:29 +0700

    first commit

---
+++ testFPS.py
@@ -0,0 +1,84 @@
+# USAGE
+# python pi_face_recognition.py --cascade haarcascade_frontalface_default.xml --encodings encodings.pickle
+
+# import the necessary packages
+from imutils.video import VideoStream
+from imutils.video import FPS
+import face_recognition
+import argparse
+import imutils
+import pickle
+import time
+import cv2
+
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-c", "--cascade", required=True,
+	help = "path to where the face cascade resides")
+ap.add_argument("-e", "--encodings", required=True,
+	help="path to serialized db of facial encodings")
+args = vars(ap.parse_args())
+
+# load the known faces and embeddings along with OpenCV's Haar
+# cascade for face detection
+print("[INFO] loading encodings + face detector...")
+data = pickle.loads(open(args["encodings"], "rb").read())
+detector = cv2.CascadeClassifier(args["cascade"])
+vs = VideoStream(src=0).start()
+# vs = VideoStream(usePiCamera=True).start()
+time.sleep(2.0)
+
+# start the FPS counter
+fps = FPS().start()
+
+# loop over frames from the video file stream
+while True:
+	# grab the frame from the threaded video stream and resize it
+	# to 500px (to speedup processing)
+	frame = vs.read()
+	frame = imutils.resize(frame, width=500)
+	
+	# convert the input frame from (1) BGR to grayscale (for face
+	# detection) and (2) from BGR to RGB (for face recognition)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+	# detect faces in the grayscale frame
+	rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
+		minNeighbors=5, minSize=(30, 30),
+		flags=cv2.CASCADE_SCALE_IMAGE)
+
+	# OpenCV returns bounding box coordinates in (x, y, w, h) order
+	# but we need them in (top, right, bottom, left) order, so we
+	# need to do a bit of reordering
+	boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rects]
+
+	# compute the facial embeddings for each face bounding box
+	encodings = face_recognition.face_encodings(rgb, boxes)
+
+	# loop over the recognized faces
+	for (top, right, bottom, left) in boxes:
+		# draw the predicted face name on the image
+		cv2.rectangle(frame, (left, top), (right, bottom),
+			(0, 255, 0), 2)
+
+	# display the image to our screen
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+	# update the FPS counter
+	fps.update()
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
